f1d8c17f4f87473021c535f646df1e16f3b25e91
Luis Miguel García <linten42@gmail.com>
2023-02-18 11:59:17 +0100

    Update bookmarks_screen.dart

--- grate_mate/lib/screens/bookmarks_screen.dart
+++ grate_mate/lib/screens/bookmarks_screen.dart
@@ -1,4 +1,6 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import '../global_information//globalRecipes.dart' as globalRecipes;
+import '../global_information/colors_palette.dart';
 
 class Bookmark extends StatefulWidget {
   const Bookmark({Key? key}) : super(key: key);
@@ -8,8 +10,176 @@ class Bookmark extends StatefulWidget {
 }
 
 class _BookmarkState extends State<Bookmark> {
+
+  Widget recipeCard(recipe){
+    return Center(
+      child: GestureDetector(
+        onTap: () {
+          Navigator.pushNamed(context, '/recipe', arguments: recipe);
+        },
+        child: Card(
+          margin: const EdgeInsets.fromLTRB(10, 0, 10, 10),
+          shape:  RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Column(
+            crossAxisAlignment:CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.fromLTRB(8, 12, 8, 12),
+                child: Text(
+                  recipe.name,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,),
+                ),
+              ),
+              AspectRatio(
+                  aspectRatio: 16 / 9,
+                  child: Image.asset(recipe.imageURL,
+                      fit: BoxFit.cover
+                  )
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(8, 12, 8, 8),
+                child: Text(recipe.description),
+              ),
+              //This code create a bar instead of a rectangles
+              /*Padding(
+                padding: const EdgeInsets.fromLTRB(8, 4, 20, 8),
+                child:  FractionallySizedBox(
+                    alignment: Alignment.centerLeft,
+                    widthFactor: recipe.difficulty / 5,
+                    child: Container(
+                      height: 10,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                        gradient: LinearGradient(
+                          colors: _getDifficultyColors(recipe.difficulty),
+                        ),
+                      ),
+                    )
+                ),
+              ),*/
+              Padding(
+                padding: const EdgeInsets.fromLTRB(8, 4, 8, 8),
+                child: getDfficultyBar(recipe.difficulty),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(8, 4, 8, 8),
+                child: Row(
+                  children: [
+                    Icon(Icons.access_time),
+                    SizedBox(width: 5),
+                    Text("${recipe.time} min"),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget getDfficultyBar(int difficulty){
+    return Row(
+      children: List.generate(
+        difficulty,
+            (index) => Container(
+          width: 30,
+          height: 15,
+          margin: EdgeInsets.only(right: 5),
+          decoration: BoxDecoration(
+            color: _getDifficultyColors(index+1),
+            borderRadius: BorderRadius.circular(5),
+          ),
+        ),
+      ),
+    );
+  }
+
+  /*List<Color> _getDifficultyColors(int difficulty) {
+    switch (difficulty) {
+      case 1:
+        return [Colors.green, Colors.green];
+      case 2:
+        return [Colors.green, const Color(0xffC6CE00)];
+      case 3:
+        return [Colors.green, const Color(0xffC6CE00), Colors.yellow];
+      case 4:
+        return [Colors.green, const Color(0xffC6CE00), Colors.yellow, Colors.orange];
+      case 5:
+        return [Colors.green, const Color(0xffC6CE00), Colors.yellow, Colors.orange, Colors.red];
+      default:
+        return [Colors.grey, Colors.grey];
+    }
+  }*/
+
+  Color _getDifficultyColors(int difficulty) {
+    switch (difficulty) {
+      case 1:
+        return Colors.green;
+      case 2:
+        return const Color(0xffC6CE00);
+      case 3:
+        return Colors.yellow;
+      case 4:
+        return Colors.orange;
+      case 5:
+        return Colors.red;
+      default:
+        return Colors.grey;
+    }
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      //backgroundColor: GrateMate.earthYellow[400],
+      backgroundColor: GrateMate.grayGrateMate,
+      body: Container(
+        margin: const EdgeInsets.fromLTRB(20.0, 60.0, 20.0, 20.0),
+        child: Column(
+          children: <Widget>[
+            Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.fromLTRB(10, 20, 10, 10),
+                  child: Row(
+                    children: const [
+                      // Icon of bookmark
+                      Icon(Icons.bookmark, color: GrateMate.darkGrateMate , size: 40),
+                      Text(
+                        'Your bookmarks',
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontFamily: 'MontserratExtraBold',
+                          //color: GrateMate.darkGrateMate
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Flexible(
+              /*child: GridView.count(
+                crossAxisCount: 1,
+                scrollDirection: Axis.vertical,
+                children: globalRecipes.recipes.map((recipe) => recipeCard(recipe)).toList(),
+                ),*/
+              child: ListView.builder(
+                itemCount: globalRecipes.recipes.length,
+                itemBuilder: (context, index) {
+                  return recipeCard(globalRecipes.recipes[index]);
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
