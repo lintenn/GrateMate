23ed2fe2f6bb9cb75adbe0ebcdfd7d1ee99b86c9
beatr <madebybee96@gmail.com>
2023-03-25 03:50:12 -1000

    remove debug banner

--- grate_mate/lib/main.dart
+++ grate_mate/lib/main.dart
@@ -22,6 +22,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'GrateMate - Recipes for everyone',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
         //
--- grate_mate/lib/models/ingredient.dart
+++ grate_mate/lib/models/ingredient.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class Ingredient{
